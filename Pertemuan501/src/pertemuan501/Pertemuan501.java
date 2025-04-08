0cacf2cb54e917b2098e7ba3446c468cd2fc293f
OOP-J <arthawebid@gmail.com>
2025-04-08 20:13:10 +0800

    Update Project

---
+++ Pertemuan501/src/pertemuan501/Pertemuan501.java
@@ -0,0 +1,17 @@
+package pertemuan501;
+
+/**
+ *
+ * @author Made Artha
+ * TGL: 8 April 2025
+ */
+public class Pertemuan501 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
